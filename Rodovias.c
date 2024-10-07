5d40dbc3b4aa7ad842ffc7747d71ac6f1be06de4
Gustavo <gustavo24104@gmail.com>
2024-10-07 19:40:21 -0300

    commit nos arquivos Rodovias

---
+++ Rodovias.c
@@ -0,0 +1,1 @@
+#include "Rodovias.h"
---
+++ Rodovias.h
@@ -0,0 +1,22 @@
+#ifndef RODOVIAS_AED1_RODOVIAS_H
+
+/* Sugestão de structs:
+Rodovia:
+ Cidades conectadas
+ Pedagios
+ Tamanho
+ Nome
+
+Cidade:
+ Proxima cidade
+ Cidade anterior
+ População
+ Distancia ate proxima
+
+No mais, por enquanto pensei na cidade serem elementos e as rodovias as listas, a rodovia começa e termina em cidades
+ especificas (no descritor), e cada rodovia eh uma lista encadeada com cada cidade
+
+ */
+#define RODOVIAS_AED1_RODOVIAS_H
+
+#endif //RODOVIAS_AED1_RODOVIAS_H
