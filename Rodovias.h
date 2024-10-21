9a888d39e57cf695a4e65313f63fcd7c7281459e
Gustavo <gustavo24104@gmail.com>
2024-10-21 11:43:16 -0300

    a

--- Rodovias.h
+++ Rodovias.h
@@ -1,22 +1,5 @@
 #ifndef RODOVIAS_AED1_RODOVIAS_H
 
-/* Sugestão de structs:
-Rodovia:
- Cidades conectadas
- Pedagios
- Tamanho
- Nome
-
-Cidade:
- Proxima cidade
- Cidade anterior
- População
- Distancia ate proxima
-
-No mais, por enquanto pensei na cidade serem elementos e as rodovias as listas, a rodovia começa e termina em cidades
- especificas (no descritor), e cada rodovia eh uma lista encadeada com cada cidade
-
- */
 #define RODOVIAS_AED1_RODOVIAS_H
 
 struct Cidade {
